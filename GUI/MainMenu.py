d90debc84eeef1eca0a0aa4762eccc6f01c67997
vultar150 <vultar1500@gmail.com>
2020-05-13 17:43:15 +0300

    add window of parameters

--- GUI/MainMenu.py
+++ GUI/MainMenu.py
@@ -1,5 +1,32 @@
 import tkinter
 
+def paramWindow(*args, **kwargs):
+    win = tkinter.Tk()
+    win.title('Parameters window')
+ 
+    tkinter.Label(win, text="Текст0:").grid(row=0, column=0, sticky='W', padx=10, pady=10)
+    winParam = tkinter.Spinbox(win, width=7, from_=0, to=100)
+    winParam.grid(row=0, column=1, padx=10)
+    tkinter.Label(win, text="Текст1:").grid(row=0, column=2, sticky='E')
+    winParam = tkinter.Spinbox(win, width=7, from_=0, to=100)
+    winParam.grid(row=0, column=3, sticky='E', padx=10)
+
+    tkinter.Label(win, text="Текст2:").grid(row=1, column=0, sticky='W', padx=10, pady=10)
+    winParam = tkinter.Spinbox(win, width=7, from_=0, to=100)
+    winParam.grid(row=1, column=1, padx=10)
+    tkinter.Label(win, text="Текст3:").grid(row=1, column=2, sticky='E')
+    winParam = tkinter.Spinbox(win, width=7, from_=0, to=100)
+    winParam.grid(row=1, column=3, sticky='E', padx=10)
+
+    tkinter.Label(win, text="Текст4:").grid(row=2, column=0, sticky='W', padx=10, pady=10)
+    winParam = tkinter.Spinbox(win, width=7, from_=0, to=100)
+    winParam.grid(row=2, column=1, padx=10)
+    tkinter.Label(win, text="Текст5:").grid(row=2, column=2, sticky='E')
+    winParam = tkinter.Spinbox(win, width=7, from_=0, to=100)
+    winParam.grid(row=2, column=3, sticky='E', padx=10)
+    
+    tkinter.Button(win, text="Фиксировать").grid(row=3, column=1, columnspan=2)
+
 def StartMenu():
     mainWindow = tkinter.Tk()
     mainWindow.title('LIFEHUB')
@@ -17,6 +44,7 @@ def StartMenu():
 
     ParamBtn = tkinter.Button(mainWindow, text = 'Задать параметры вселенной', font = 'Arial 24', bd = 5, width = 25)
     ParamBtn.grid(row = 3, column = 2, padx = 20, pady = 20)
+    ParamBtn.bind('<Button>', paramWindow)
 
     Btn = tkinter.Button(mainWindow, text = 'Начать симуляцию', font = 'Arial 24', bd = 5, width = 25)
     Btn.grid(row = 4, column = 2, padx = 20, pady = 20)
