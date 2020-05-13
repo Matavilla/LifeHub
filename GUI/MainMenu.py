4f2c24ecf0a32f8c9ea19a104e9fc10072bdb4e9
vultar150 <vultar1500@gmail.com>
2020-05-13 19:14:57 +0300

    save all param from parameters window

--- GUI/MainMenu.py
+++ GUI/MainMenu.py
@@ -1,26 +1,86 @@
 import tkinter
 
+
+class WorldParameters:
+    '''Сохранение параметров вселенной'''
+    def __init__(self, tik = 0,    chaosMoment = 0,
+                 amountOfFood = 0, amountOfPoison = 0,
+                 biom1 = 0,        biom2 = 0,
+                 biom3 = 0):
+        self.tikOfUniverse = tik
+        self.chaosMoment = chaosMoment
+        self.amountOfFood = amountOfFood
+        self.amountOfPoison = amountOfPoison
+        #generation period of food and poison
+        self.biom1 = biom1
+        self.biom2 = biom2
+        self.biom3 = biom3
+
+
+    def check(self):
+        print("Проверка параметров")
+
+
+parameters = WorldParameters()
+
 def addStr(win, _text1, _text2, _row, _from = 0, _to = 100):
+    ''' Вспомогательная функция добавления строки
+        таблицы в окно с параметрами'''
     tkinter.Label(win, text=_text1).grid(row=_row, column=0, sticky='W', padx=10, pady=10)
-    winParam = tkinter.Spinbox(win, width=7, from_=_from, to=_to)
-    winParam.grid(row=_row, column=1, padx=10)
+    winParam1 = tkinter.Spinbox(win, width=7, from_=_from, to=_to)
+    winParam1.grid(row=_row, column=1, padx=10)
     tkinter.Label(win, text=_text2).grid(row=_row, column=2, sticky='E')
-    winParam = tkinter.Spinbox(win, width=7, from_=_from, to=_to)
-    winParam.grid(row=_row, column=3, sticky='E', padx=10)
+    winParam2 = tkinter.Spinbox(win, width=7, from_=_from, to=_to)
+    winParam2.grid(row=_row, column=3, sticky='E', padx=10)
+    return winParam1, winParam2
 
 def paramWindow(*args, **kwargs):
+    '''Создание окна с параметрами вселенной и сохранение параметров'''
+    def saveParameters(*args, **kwargs):
+        global parameters
+        parameters.tikOfUniverse = tik.get()
+        parameters.chaosMoment = chaos.get()
+        parameters.amountOfFood = food.get()
+        parameters.amountOfPoison = poison.get()
+        parameters.biom1 = biom1.get()
+        parameters.biom2 = biom2.get()
+        parameters.biom3 = biom3.get()
+
     win = tkinter.Tk()
     win.title('Parameters window')
 
-    addStr(win, "Тик вселенной", "Момент хаоса", 0, 10, 100000)
-    addStr(win, "Кол-во еды", "Кол-во яда:", 1, 0, 1000)
+    tik, chaos = addStr(win, "Тик вселенной", "Момент хаоса", 0, 0, 1000)
+    food, poison = addStr(win, "Кол-во еды", "Кол-во яда:", 1, 0, 1000)
+
     tkinter.Label(win, text="Период генерации еды/яда").grid(row=2, column=1, sticky='W', columnspan=2)
-    addStr(win, "Биом 1:", "Биом 2:", 3)
+
+    biom1, biom2 = addStr(win, "Биом 1:", "Биом 2:", 3)
+
     tkinter.Label(win, text="Биом 3:").grid(row=4, column=0, sticky='W', padx=10, pady=10)
-    winParam = tkinter.Spinbox(win, width=7, from_=0, to=100)
-    winParam.grid(row=4, column=1, padx=10)
+
+    biom3 = tkinter.Spinbox(win, width=7, from_=0, to=100)
+    biom3.grid(row=4, column=1, padx=10)
     
-    tkinter.Button(win, text="Фиксировать").grid(row=5, column=1, columnspan=2)
+    button = tkinter.Button(win, text="Сохнанить")
+    button.grid(row=5, column=1, columnspan=2)
+    button.bind('<Button>', saveParameters)
+
+
+# def saveParameters(*args, **kwargs):
+#     global parameters
+#     parameters.
+
+def printParam(*args, **kwargs):
+    global parameters
+    print(f'tikOfUniverse = {parameters.tikOfUniverse}')
+    print(f'chaosMoment = {parameters.chaosMoment}')
+    print(f'amountOfFood = {parameters.amountOfFood}')
+    print(f'amountOfPoison = {parameters.amountOfPoison}')
+    print(f'biom1 = {parameters.biom1}')
+    print(f'biom2 = {parameters.biom2}')
+    print(f'biom3 = {parameters.biom3}')
+    print('\n')
+
 
 def StartMenu():
     mainWindow = tkinter.Tk()
@@ -37,11 +97,14 @@ def StartMenu():
     startBtn = tkinter.Button(mainWindow, text = 'Начать симуляцию', font = 'Arial 24', bd = 5, width = 25)
     startBtn.grid(row = 2, column = 2, padx = 20, pady = 20)
 
+
     ParamBtn = tkinter.Button(mainWindow, text = 'Задать параметры вселенной', font = 'Arial 24', bd = 5, width = 25)
     ParamBtn.grid(row = 3, column = 2, padx = 20, pady = 20)
     ParamBtn.bind('<Button>', paramWindow)
 
     Btn = tkinter.Button(mainWindow, text = 'Начать симуляцию', font = 'Arial 24', bd = 5, width = 25)
     Btn.grid(row = 4, column = 2, padx = 20, pady = 20)
+
+    startBtn.bind('<Button>', printParam)
 
     mainWindow.mainloop()
