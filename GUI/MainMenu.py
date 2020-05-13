5eacf9e4ee5359f257e4a62f1e6485cc4a8a094f
vultar150 <vultar1500@gmail.com>
2020-05-13 18:13:08 +0300

    change names of parameters window

--- GUI/MainMenu.py
+++ GUI/MainMenu.py
@@ -1,22 +1,26 @@
 import tkinter
 
-def addStr(win, _text1, _text2, _row):
+def addStr(win, _text1, _text2, _row, _from = 0, _to = 100):
     tkinter.Label(win, text=_text1).grid(row=_row, column=0, sticky='W', padx=10, pady=10)
-    winParam = tkinter.Spinbox(win, width=7, from_=0, to=100)
+    winParam = tkinter.Spinbox(win, width=7, from_=_from, to=_to)
     winParam.grid(row=_row, column=1, padx=10)
     tkinter.Label(win, text=_text2).grid(row=_row, column=2, sticky='E')
-    winParam = tkinter.Spinbox(win, width=7, from_=0, to=100)
+    winParam = tkinter.Spinbox(win, width=7, from_=_from, to=_to)
     winParam.grid(row=_row, column=3, sticky='E', padx=10)
 
 def paramWindow(*args, **kwargs):
     win = tkinter.Tk()
     win.title('Parameters window')
 
-    addStr(win, "Текст0:", "Текст1:", 0)
-    addStr(win, "Текст2:", "Текст3:", 1)
-    addStr(win, "Текст4:", "Текст5:", 2)
+    addStr(win, "Тик вселенной", "Момент хаоса", 0, 10, 100000)
+    addStr(win, "Кол-во еды", "Кол-во яда:", 1, 0, 1000)
+    tkinter.Label(win, text="Период генерации еды/яда").grid(row=2, column=1, sticky='W', columnspan=2)
+    addStr(win, "Биом 1:", "Биом 2:", 3)
+    tkinter.Label(win, text="Биом 3:").grid(row=4, column=0, sticky='W', padx=10, pady=10)
+    winParam = tkinter.Spinbox(win, width=7, from_=0, to=100)
+    winParam.grid(row=4, column=1, padx=10)
     
-    tkinter.Button(win, text="Фиксировать").grid(row=3, column=1, columnspan=2)
+    tkinter.Button(win, text="Фиксировать").grid(row=5, column=1, columnspan=2)
 
 def StartMenu():
     mainWindow = tkinter.Tk()
