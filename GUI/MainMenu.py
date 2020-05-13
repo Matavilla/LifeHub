aa2e1c15436d56b9f99f053610cef8a87237af17
Matavilla <mr.andrej1102@yandex.ru>
2020-05-13 14:22:06 +0300

    first simple menu

--- GUI/MainMenu.py
+++ GUI/MainMenu.py
@@ -1,4 +1,9 @@
 import tkinter
 
 def StartMenu():
-    tkinter._test()
+    mainWindow = tkinter.Tk()
+    mainWindow.title('LIFEHUB')
+    mainWindow.geometry('640x480')
+    startBtn = tkinter.Button(mainWindow, text = 'Начать симуляцию', font = 'Arial 24')
+    startBtn.grid(row="5", column="10", rowspan = "2", columnspan = "5")
+    mainWindow.mainloop()
