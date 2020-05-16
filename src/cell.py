5207a58a5519e5b51d14d5bc9ed5076fb5e3177a
vultar150 <vultar1500@gmail.com>
2020-05-17 01:00:28 +0300

    add module cell that basic class of all objects

---
+++ src/cell.py
@@ -0,0 +1,10 @@
+import pygame
+
+class Cell:
+    def __init__(self, x: int, y: int, 
+                cell_size: int, alive: bool=False) -> None:
+        self.Bounds = pygame.Rect(x, y, cell_size, cell_size)
+        self.X = x
+        self.Y = y
+        self.CellSize = cell_size
+        self.Alive = alive
