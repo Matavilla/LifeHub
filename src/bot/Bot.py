5cec87bbd0eefa5e9d9c7e8a02b1b19fd8f87dbe
vultar150 <vultar1500@gmail.com>
2020-06-07 19:36:31 +0300

    reduce number of jumps

--- src/bot/Bot.py
+++ src/bot/Bot.py
@@ -26,18 +26,18 @@ class Bot:
         self.Color = self.Biom_bot_color[self.Dna.Biom]
 
     def get_dir_and_action(self):
-        """ 0..31 - move
-            32..63 - attack
-            64..95 - check
-            96..127 - rotate
-            128..255 - jump
+        """ 0..39 - move
+            40..79 - attack
+            80..119 - check
+            120..159 - rotate
+            160..255 - jump
 
         """
         curr_command = self.Ai.Gens[self.Pointer_of_ai]
         dx, dy, action = 0, 0, None
         max_num_of_actions = 10
-        while 96 <= curr_command <= 255 and max_num_of_actions:
-            if 96 <= curr_command <= 127:
+        while 120 <= curr_command <= 255 and max_num_of_actions:
+            if 120 <= curr_command <= 159:
                 action = "rotate"
                 self.Curr_direction = (curr_command + 
                                        self.Curr_direction -
@@ -48,11 +48,11 @@ class Bot:
             curr_command = self.Ai.Gens[self.Pointer_of_ai]
             max_num_of_actions -= 1
 
-        if 0 <= curr_command <= 31:
+        if 0 <= curr_command <= 39:
             action = "move"
-        elif 32 <= curr_command <= 63:
+        elif 40 <= curr_command <= 79:
             action = "attack"
-        elif 64 <= curr_command <= 95:
+        elif 80 <= curr_command <= 119:
             action = "check"
         else:
             action = None
