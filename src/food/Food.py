46257d525f70cee6bd8d6728646ff89c063dcd9d
vultar150 <vultar1500@gmail.com>
2020-06-09 15:08:11 +0300

    Change food and toxic bounds for each biom.

--- src/food/Food.py
+++ src/food/Food.py
@@ -1,8 +1,8 @@
 import random
 
 class Food:
-    Biom_food_bound = {1 : (0, 40), 2 : (0, 70), 3 : (0, 100)}
-    Biom_toxic_bound = {1 : (0, 20), 2 : (0, 40), 3 : (0, 100)}
+    Biom_food_bound = {1 : (0, 200), 2 : (0, 100), 3 : (0, 30)}
+    Biom_toxic_bound = {1 : (0, 30), 2 : (0, 100), 3 : (0, 200)}
 
     def __init__(self, biom):
         self.Food_value = random.randint(*self.Biom_food_bound[biom])
