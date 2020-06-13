0ce6de5591fcf696921fdf4a1689fa7e3e188309
Volodimirich <shaposh-vova@rambler.ru>
2020-06-13 14:33:39 -0700

    Eng local added

--- LifeHub.py
+++ LifeHub.py
@@ -1,6 +1,10 @@
 import GUI
 import src
+import os
+import sys
+import gettext
 
+gettext.install("locale", os.path.dirname(sys.argv[0]), names=("ngettext",))
 wPar = GUI.WorldParameters()
 GUI.StartMenu(wPar)
 if not wPar:
