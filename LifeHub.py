9d0d478ccd1f1313dff8cee637d5aacc5434837f
vultar150 <vultar1500@gmail.com>
2020-06-13 23:37:21 +0300

    fix bug with start LifeHub after build docs

--- LifeHub.py
+++ LifeHub.py
@@ -1,12 +1,17 @@
 import GUI
 import src
 
-wPar = GUI.WorldParameters()
-GUI.StartMenu(wPar)
-if not wPar:
-    print("[ERROR]     WrongParametersWorld")
-    raise SystemExit(1)
-wPar.print_in_log()
-handler = src.Handler(wPar)
-handler.create_world()
-GUI.StartGame(handler)
+def main():
+
+    wPar = GUI.WorldParameters()
+    GUI.StartMenu(wPar)
+    if not wPar:
+        print("[ERROR]     WrongParametersWorld")
+        raise SystemExit(1)
+    wPar.print_in_log()
+    handler = src.Handler(wPar)
+    handler.create_world()
+    GUI.StartGame(handler)
+
+if __name__ == '__main__':
+    main()
