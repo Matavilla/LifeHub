496ea643d244c668bef05a1b8f74bf30014beb6f
vultar150 <vultar1500@gmail.com>
2020-06-14 18:21:05 +0300

    fix locale

--- setup.py
+++ setup.py
@@ -1,33 +1,22 @@
 from setuptools import setup, find_packages
 
+try:
+    readme = open("README.md")
+    long_description = str(readme.read())
+finally:
+    readme.close()
 
 setup(
     name="LifeHub",
     version="0.0",
     packages=find_packages(),
-
-    # Project uses reStructuredText, so ensure that the docutils get
-    # installed or upgraded on the target machine
+    long_description=long_description,
     install_requires=["pygame"],
-
     package_data={
-        # If any package contains *.txt or *.rst files, include them:
-        "": ["*.rst", "*.md"],
+        "LifeHub": ["en/LC_MESSAGES/*.mo", "pl/LC_MESSAGES/*.mo"],
     },
-
     # metadata to display on PyPI
     author="Владимир Шапошников, Владимир Рябченков, Андрей Морквин",
-    #author_email="me@example.com",
     description="This is LifeHub package",
     url="https://github.com/Matavilla/LifeHub",  # project home page, if any
-    # project_urls={
-    #     "Bug Tracker": "https://bugs.example.com/HelloWorld/",
-    #     "Documentation": "https://docs.example.com/HelloWorld/",
-    #     "Source Code": "https://code.example.com/HelloWorld/",
-    # },
-    # classifiers=[
-    #     "License :: OSI Approved :: Python Software Foundation License"
-    # ]
-
-    # could also include long_description, download_url, etc.
 )
